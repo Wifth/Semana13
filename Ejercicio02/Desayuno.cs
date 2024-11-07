88b001b77128d7321828d91d8f7fe4855dac3f0f
Wifth <alexander.obando.os2206@gmail.com>
2024-11-07 09:42:11 -0500

    Ejercicio 02

--- Ejercicio02/Desayuno.cs
+++ Ejercicio02/Desayuno.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Ejercicio02
 {
-    internal class Desayuno
+    public class Desayuno
     {
         public string Nombre { get; set; }
         public string Dias { get; set; }
